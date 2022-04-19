c44621f35eed67dab18549b0656ea0843af92796
luisdomian <luismdomian@gmail.com>
2022-04-18 22:39:01 -0600

    tarea 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Luis Domian - carné: B62377
@@ -8,4 +8,3 @@ function App() {
   );
 }
 
-export default App;
